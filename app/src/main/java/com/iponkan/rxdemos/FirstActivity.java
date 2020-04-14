ddab4642289ca611f5c9ec49374a77c7c9a2d47d
iponkan <iponkan@gmail.com>
2020-04-14 21:42:31 +0800

    掌握interval的用法

---
+++ app/src/main/java/com/iponkan/rxdemos/FirstActivity.java
@@ -0,0 +1,73 @@
+package com.iponkan.rxdemos;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.trello.rxlifecycle3.components.support.RxAppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class FirstActivity extends RxAppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+    ;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        test();
+    }
+
+    @SuppressLint("CheckResult")
+    private void test() {
+//        mCompositeDisposable.add();
+        Observable.interval(0, 500, TimeUnit.MILLISECONDS)
+                .take(10)
+                // map操作的书写位置要注意，如果是在线程里执行需要写在observeOn前面
+                .map(new Function<Long, Object>() {
+                    @Override
+                    public Object apply(Long aLong) throws Exception {
+                        Log.d(TAG, "Thread;" + Thread.currentThread().getName() + " " + Thread.currentThread().getId());
+                        return System.currentTimeMillis();
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableObserver<Object>() {
+                    @Override
+                    public void onNext(Object o) {
+                        Log.d(TAG, "数据:" + o);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, e.toString(), e);
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 如果退出程序，就清除后台任务
+        mCompositeDisposable.clear();
+    }
+}
--- app/src/main/java/com/iponkan/rxdemos/MainActivity.java
+++ app/src/main/java/com/iponkan/rxdemos/MainActivity.java
@@ -1,14 +1,29 @@
 package com.iponkan.rxdemos;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.trello.rxlifecycle3.components.support.RxAppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends RxAppCompatActivity {
+
+    @BindView(R.id.tv_hello)
+    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_hello)
+    public void onViewClicked() {
+        startActivity(new Intent(this, FirstActivity.class));
     }
 }
